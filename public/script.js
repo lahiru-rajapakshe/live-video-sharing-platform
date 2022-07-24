f25bed2c252f436963d7e0c51bd2d787bb1e4acd
LahiruRJ <sanlahiru720@gmail.com>
2022-07-24 13:48:13 +0530

    Add function connectToNewUser

--- public/script.js
+++ public/script.js
@@ -16,3 +16,16 @@ myVideo.muted=true;
 socket.on('user-disconnected',userId=>{
     console.log(userId)
 })
+
+function connectToNewUser(userId,stream){
+    const call=myPeer.call(userId,stream)
+    const video = document.createElement('video');
+
+    call.on('stream',userVideoStream=>{
+        addVideoStream(video,userVideoStream)
+    })
+    call.on('close',()=>{
+        video.remove();
+    })
+
+}
