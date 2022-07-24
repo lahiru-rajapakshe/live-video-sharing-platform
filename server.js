52c9b8a6929686c3d738e88ed44c3a80a9d52595
LahiruRJ <sanlahiru720@gmail.com>
2022-07-24 09:57:07 +0530

    Set roomId:req.params

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express= require('express');
-
+// const { execPath, hasUncaughtExceptionCaptureCallback } = require('process');
 const app=express();
 const server =require('http').Server(app);
 const io=require('socket.io')(server);
 const { v4: uuidV4 } = require('uuid')
+// import {v4 as uuidv4} from 'uuid';
+var socket      = require('socket.io')
 
 app.set('view engine','ejs')
 app.use(express.static('public'));
@@ -12,13 +14,11 @@ app.get('/',(req,res)=>{
 res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/',(req,res)=>{
-    res.redirect(`/${uuidV4()}`)
-    })
-
 app.get('/:room',(req,res)=>{
     res.render('room',{ roomId: req.params.room })
 })
+
+
 
 
 server.listen(5500);
