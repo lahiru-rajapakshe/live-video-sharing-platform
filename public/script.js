ec249d023a483e4f71e9291bb9ad05bc51536f1d
LahiruRJ <sanlahiru720@gmail.com>
2022-07-24 16:06:07 +0530

    Refactor script.js

--- public/script.js
+++ public/script.js
@@ -11,12 +11,25 @@ const myPeer=new Peer(undefined,{
 const myVideo = document.createElement('video');
 myVideo.muted=true;
 
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
 
+}).then(stream=>{
+addVideoStream(myVideo,stream)
 
-socket.on('user-disconnected',userId=>{
-    console.log(userId)
+    myPeer.on('call',call=>{
+        call.answer(stream)
+const video = document.createElement('video');
+        call.on('stream',userVideoStream=>{
+           addVideoStream(video,userVideoStream)
+        })
+    })
+
+    socket.on('user-connected',userId=>{
+        connectToNewUser(userId,stream)
+    })
 })
-
 function connectToNewUser(userId,stream){
     const call=myPeer.call(userId,stream)
     const video = document.createElement('video');
@@ -38,6 +51,7 @@ function addVideoStream(video,stream){
 
     videoGrid.append(video)
 }
+
 
 myPeer.on('open',id=>{
     socket.emit('join-room',ROOM_ID,id);
