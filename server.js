896c04788364a397d72a937ae0a04c5968dfbe23
LahiruRJ <sanlahiru720@gmail.com>
2022-07-24 09:20:27 +0530

    Set roomId

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app=express();
 const server =require('http').Server(app);
 const io=require('socket.io')(server);
 
+app.set('view engine','ejs')
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+res.redirect(`/${uuidV4()}`)
+})
+
+app.get('/:room',(req,res)=>{
+    res.render('room',{ roomId: req.params.room })
+})
 
 
 server.listen(5500);
